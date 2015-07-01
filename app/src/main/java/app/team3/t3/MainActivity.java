c8ad65acfc00de43002630229ada37a97b3ad5ee
sssbug <sssbug@gmail.com>
2015-07-01 13:42:00 -0700

    init commit

--- app/src/main/java/app/team3/t3/MainActivity.java
+++ app/src/main/java/app/team3/t3/MainActivity.java
@@ -1,17 +1,63 @@
 package app.team3.t3;
 
+import android.database.DatabaseUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Restaurant res;
+    ResDatabaseHelper resDB;
+    private Button b1;
+    private Button b2;
+    private TextView t1;
+    private ResDatabaseHelper.ResCursor mCursor;
+    private DatabaseUtils a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res = new Restaurant("abc", 5, "4153382929", "Chinese", "1600 holloway", "San Francisco", 94110, 0, 0);
+        resDB = new ResDatabaseHelper(getApplicationContext());
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        t1 = (TextView) findViewById(R.id.textView2);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t1.setText("12312312312");
+                resDB.insertRes(res);
+                t1.setText("insert success");
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rn = new Random();
+                int Random_Number = rn.nextInt(10) + 1;
+                mCursor = resDB.queryRes(Random_Number);
+                t1.setText(DatabaseUtils.dumpCursorToString(mCursor));
+
+
+            }
+        });
+
+
     }
 
     @Override
@@ -35,4 +81,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
