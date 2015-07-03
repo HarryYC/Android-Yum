603e10b18e04121316d0b206f2229536ba63f7cc
sssbug <sssbug@gmail.com>
2015-07-03 12:39:08 -0700

    Change SensorListener to SensorEventListner Clean up the MainActivity, add comments

--- app/src/main/java/app/team3/t3/MainActivity.java
+++ app/src/main/java/app/team3/t3/MainActivity.java
@@ -1,8 +1,10 @@
 package app.team3.t3;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.hardware.SensorListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,28 +16,87 @@ import android.widget.TextView;
 import java.util.Random;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
     ResDatabaseHelper resDB;
-    private ShakeListener mShaker;
     private Button b1;
     private Button b2;
     private TextView t1;
 
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 24) {
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(500);
+                Random rn = new Random();
+                int Random_Number = rn.nextInt(20) + 1;
+                Restaurant randromRestaurant = resDB.getRestaurant(Random_Number);
+                String restaurantOutput = "Name: " + randromRestaurant.getName()
+                        + "\nRating: " + randromRestaurant.getRating()
+                        + "\nPhone: " + randromRestaurant.getPhone()
+                        + "\nCategories: " + randromRestaurant.getCategories()
+                        + "\n Address: " + randromRestaurant.getAddress()
+                        + "\nCity: " + randromRestaurant.getCity()
+                        + "\nZipcode: " + randromRestaurant.getZipcode()
+                        + "\nLatitude: " + randromRestaurant.getLatitude()
+                        + "\nLongitude: " + randromRestaurant.getLongitude();
+                t1.setText(restaurantOutput);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    //--------------------------Sensor Listener--------------------------//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Context context = getApplicationContext();
 
+
+        //--------------------------Yelp-------------------------------//
+
+        final Context context = getApplicationContext();
         final YelpSearch mySearch = new YelpSearch(context);
-        mySearch.defaultSearch();
         final Restaurant[] allRestaurant = mySearch.getRestaurant();
+        mySearch.defaultSearch();
+
+        //--------------------------Shake Sensor-----------------------//
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+
+        //--------------------------Database----------------------------//
 
         resDB = new ResDatabaseHelper(context);
+
+        //--------------------------Components---------------------------//
 
         b1 = (Button) findViewById(R.id.button);
         b2 = (Button) findViewById(R.id.button2);
         t1 = (TextView) findViewById(R.id.textView2);
+
+        //--------------------------Listeners----------------------------//
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,34 +124,37 @@ public class MainActivity extends ActionBarActivity {
                         + "\nZipcode: " + randromRestaurant.getZipcode()
                         + "\nLatitude: " + randromRestaurant.getLatitude()
                         + "\nLongitude: " + randromRestaurant.getLongitude();
-
-
                 t1.setText(restaurantOutput);
             }
         });
+    } //end onCreat
 
-        final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-        mShaker = new ShakeListener(this);
-        mShaker.setOnShakeListener(new ShakeListener.OnShakeListener() {
-            public void onShake() {
-                vibe.vibrate(300);
-                Random rn = new Random();
-                int Random_Number = rn.nextInt(allRestaurant.length) + 1;
-                Restaurant randromRestaurant = resDB.getRestaurant(Random_Number);
-                String restaurantOutput = "Name: " + randromRestaurant.getName()
-                        + "\nRating: " + randromRestaurant.getRating()
-                        + "\nPhone: " + randromRestaurant.getPhone()
-                        + "\nCategories: " + randromRestaurant.getCategories()
-                        + "\n Address: " + randromRestaurant.getAddress()
-                        + "\nCity: " + randromRestaurant.getCity()
-                        + "\nZipcode: " + randromRestaurant.getZipcode()
-                        + "\nLatitude: " + randromRestaurant.getLatitude()
-                        + "\nLongitude: " + randromRestaurant.getLongitude();
-                t1.setText(restaurantOutput);
-            }
-        });
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
     }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mSensorListener,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -114,5 +178,13 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
 
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
