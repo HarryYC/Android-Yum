6aefdeb0732de3cae066208b450084926059414b
jeremydoan <jdoan31@gmail.com>
2015-07-04 15:13:54 -0700

    changed from using threads to asynctask and implemented a simple location service check

--- app/src/main/java/app/team3/t3/SplashScreen.java
+++ app/src/main/java/app/team3/t3/SplashScreen.java
@@ -1,33 +1,80 @@
 package app.team3.t3;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.app.Dialog;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 
 /**
  * Created by jdoan on 6/30/2015.
+ * Splash screen to appear on app start up
  */
 public class SplashScreen extends Activity {
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                   sleep(3000);
-                }catch(InterruptedException e){
-                   e.printStackTrace();
-                }finally{
-                   Intent i = new Intent("app.team3.t3.MAINACTIVITY");
-                   startActivity(i);
+
+        // Get Location Manager and check for GPS & Network location services
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            setContentView(R.layout.activity_splash);
+            showLocationPrompt();
+        }else{
+            new LoadViewTask().execute();
+        }
+    }
+
+    private void showLocationPrompt() {
+        // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Services Not Active");
+            builder.setMessage("Please enable Location Services and GPS");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
                 }
+            });
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        }
+
+    private class LoadViewTask extends AsyncTask<Void, Integer, Void>{
+        @Override
+        protected void onPreExecute(){
+
+            setContentView(R.layout.activity_splash);
+            super.onPreExecute();
+        }
+        
+        @Override
+        protected Void doInBackground(Void... params){
+            try {
+                synchronized (this){
+                    wait(3000);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                Intent i = new Intent("app.team3.t3.MAINACTIVITY");
+                startActivity(i);
             }
-        };
-        timerThread.start();
+            return null;
+        }
     }
 
     @Override
