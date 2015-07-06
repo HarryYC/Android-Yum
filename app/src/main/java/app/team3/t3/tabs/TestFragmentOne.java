82ca3809440bd1177d1bb9940c65449d0ca7d98e
Nan Den <nannandenden@gmail.com>
2015-07-06 16:44:16 -0700

    add view pager and burger menu

---
+++ app/src/main/java/app/team3/t3/tabs/TestFragmentOne.java
@@ -0,0 +1,24 @@
+package app.team3.t3.tabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.team3.t3.R;
+
+/**
+ * Created by nanden on 7/5/15.
+ */
+public class TestFragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one,container,false);
+        return view;
+
+    }
+}
